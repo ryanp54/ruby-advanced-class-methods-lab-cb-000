adc3e67d4b6dc8589dc5451c46dd4f198c5dc848
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 04:20:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,13 +7,13 @@ class Song
   end
 
   def self.create
-    song = Song.new
+    song = self.new
     song.save
     song
   end
 
   def self.new_by_name(name)
-    song = Song.new
+    song = self.new
     song.name = name
     song
   end
@@ -31,6 +31,17 @@ class Song
   def self.find_or_create_by_name(name)
     song = self.find_by_name(name)
     !!song ? song : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by(|song| song.name)    
+  end
+
+  def self.new_from_filename(filename)
+    artist, song_name = filename.split(/(\s-\s)|(.mp3))
+    song = self.new_by_name(song_name)
+    song.artist_name = artist
+    song
   end
 
   def save
